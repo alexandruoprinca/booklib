87e6ac14629ca677b57b73cc4639bc5becf3e880
alexandruoprinca <oprincaalexandru@gmail.com>
2023-08-06 12:10:11 +0300

    added repo as a state for rocket

--- src/main.rs
+++ src/main.rs
@@ -15,74 +15,62 @@ use rocket::request::FromParam;
 #[macro_use]
 extern crate rocket;
 
-#[get("/?<author>&<read>")]
-fn list(author: Option<String>, read: Option<bool>) {
-    let args = ArgumentsProviderRequest::new();
-    println!("Author is {}", author.unwrap());
-    let mut repo: Box<dyn Repository<library_entry::LibraryEntry>> =
-        Box::new(LibraryEntriesRepository::new());
-    fill_repo_with_dummy_data(&mut repo);
-    let mut command = CommandFactory::new_command(CommandType::ADD, repo.as_mut(), Box::new(args));
-
-    command.execute();
+struct App {
+    repo: Box<dyn Repository<library_entry::LibraryEntry>>
 }
 
-#[get("/?<title>&<author>")]
-fn add(title: Option<String>, author: Option<String>) {
-    let args = ArgumentsProviderRequest::new();
-    let mut repo: Box<dyn Repository<library_entry::LibraryEntry>>= Box::new(LibraryEntriesRepository::new());
-    fill_repo_with_dummy_data(&mut repo);
-    let mut command = CommandFactory::new_command(CommandType::LIST, repo.as_mut(), Box::new(args));
+mod CommandApi {
+    use crate::{
+        arguments_provider::ArgumentsProviderRequest,
+        command::CommandType,
+        command_factory::CommandFactory,
+        fill_repo_with_dummy_data, library_entry,
+        repository::{LibraryEntriesRepository, Repository},
+    };
 
-    command.execute();
+    #[get("/?<author>&<read>")]
+    pub fn list(author: Option<String>, read: Option<bool>) {
+        let args = ArgumentsProviderRequest::new();
+        println!("Author is {}", author.unwrap());
+        let mut repo: Box<dyn Repository<library_entry::LibraryEntry>> =
+            Box::new(LibraryEntriesRepository::new());
+        fill_repo_with_dummy_data(&mut repo);
+        let mut command =
+            CommandFactory::new_command(CommandType::ADD, repo.as_mut(), Box::new(args));
+
+        command.execute();
+    }
+
+    #[get("/?<title>&<author>")]
+    pub fn add(title: Option<String>, author: Option<String>) {
+        let args = ArgumentsProviderRequest::new();
+        let mut repo: Box<dyn Repository<library_entry::LibraryEntry>> =
+            Box::new(LibraryEntriesRepository::new());
+        fill_repo_with_dummy_data(&mut repo);
+        let mut command =
+            CommandFactory::new_command(CommandType::LIST, repo.as_mut(), Box::new(args));
+
+        command.execute();
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-    .mount("/list", routes![list])
-    .mount("/add", routes![add])
+    let mut repo: Box<dyn Repository<library_entry::LibraryEntry>> =
+        Box::new(LibraryEntriesRepository::new());
+
+    fill_repo_with_dummy_data(&mut repo);
+
+    let state = App{
+        repo
+    };
+
+    let _rocket = rocket::build()
+        .mount("/list", routes![CommandApi::list])
+        .mount("/add", routes![CommandApi::add])
+        .manage(state);
+    _rocket
 }
-
-// fn main() {
-//     let mut repo: Box<dyn Repository<library_entry::LibraryEntry>> =
-//         Box::new(LibraryEntriesRepository::new());
-
-//     fill_repo_with_dummy_data(&mut repo);
-
-//     let matches: clap::ArgMatches = command!()
-//         .propagate_version(true)
-//         .subcommand_required(true)
-//         .arg_required_else_help(true)
-//         .subcommand(
-//             ArgCommand::new("add")
-//                 .about("Adds files to myapp")
-//                 .arg(arg!(--"author" <String>))
-//                 .arg(arg!(--"title" <String>))
-//                 .arg(arg!(--"read")),
-//         )
-//         .subcommand(
-//             ArgCommand::new("list")
-//                 .about("List books")
-//                 .arg(arg!(--"author" <String>)),
-//         )
-//         .get_matches();
-
-//     let mut command = match matches.subcommand() {
-//         Some(("add", matches)) => {
-//             let command_args = arguments_provider::ArgumentsProviderCLI::new(matches.clone());
-//             CommandFactory::new_command(CommandType::ADD, repo.as_mut(), Box::new(command_args))
-//         }
-//         Some(("list", matches)) => {
-//             let command_args = arguments_provider::ArgumentsProviderCLI::new(matches.clone());
-//             CommandFactory::new_command(CommandType::LIST, repo.as_mut(), Box::new(command_args))
-//         }
-//         _ => unreachable!("Wrong command"),
-//     };
-
-//     command.execute();
-
-// }
 
 fn fill_repo_with_dummy_data(repo: &mut Box<dyn Repository<library_entry::LibraryEntry>>) {
     let library_entry = LibraryEntry::new(
@@ -135,3 +123,37 @@ fn fill_repo_with_dummy_data(repo: &mut Box<dyn Repository<library_entry::Librar
     );
     repo.create(library_entry);
 }
+
+
+
+// let matches: clap::ArgMatches = command!()
+    //     .propagate_version(true)
+    //     .subcommand_required(true)
+    //     .arg_required_else_help(true)
+    //     .subcommand(
+    //         ArgCommand::new("add")
+    //             .about("Adds files to myapp")
+    //             .arg(arg!(--"author" <String>))
+    //             .arg(arg!(--"title" <String>))
+    //             .arg(arg!(--"read")),
+    //     )
+    //     .subcommand(
+    //         ArgCommand::new("list")
+    //             .about("List books")
+    //             .arg(arg!(--"author" <String>)),
+    //     )
+    //     .get_matches();
+
+    // let mut command = match matches.subcommand() {
+    //     Some(("add", matches)) => {
+    //         let command_args = arguments_provider::ArgumentsProviderCLI::new(matches.clone());
+    //         CommandFactory::new_command(CommandType::ADD, repo.as_mut(), Box::new(command_args))
+    //     }
+    //     Some(("list", matches)) => {
+    //         let command_args = arguments_provider::ArgumentsProviderCLI::new(matches.clone());
+    //         CommandFactory::new_command(CommandType::LIST, repo.as_mut(), Box::new(command_args))
+    //     }
+    //     _ => unreachable!("Wrong command"),
+    // };
+
+    // command.execute();
